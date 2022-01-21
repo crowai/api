aa7bb4cdc76d42785feb62f2cab31ab66f5d3007
ikoide <ian.koide@protonmail.com>
2022-01-21 09:34:07 -0500

    Made sentiment counter and prediction date real

--- crow/main/views.py
+++ crow/main/views.py
@@ -1,8 +1,14 @@
+from datetime import datetime, timedelta
+
 from flask import Blueprint, render_template
+
+from crow.data.models import Sentiment
 
 main = Blueprint("main", __name__)
 
 @main.route("/")
 @main.route("/home")
 def home():
-  return render_template("main/index.html")
+  sentiments = len(Sentiment.query.all())
+  predictionDate = datetime.now() + timedelta(hours=1)
+  return render_template("main/index.html", sentiments=sentiments, predictionDate=predictionDate)
